d488300e30c1082efc6c09d320432be61ba042d8
MaslowskiLukasz <maslowski_lukasz@pop-os.localdomain>
2022-05-03 12:55:15 +0200

    Change App to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>CV-app</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>CV-app</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
