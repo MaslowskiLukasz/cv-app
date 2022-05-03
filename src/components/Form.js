d93c72e871ba0ebe4486e1ab64ec4ce76fffcdc0
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-05-03 13:13:44 +0200

    Add section components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Form</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>Form</h2>
+        <GeneralInfo />
+        <Education />
+        <Experience />
+      </div>
+    )
+  }
+}
+
+class GeneralInfo extends Component {
+  render() {
+    return(
+      <div>
+        <h3>GeneralInfo</h3>
+      </div>
+    )
+  }
+}
+
+class Education extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Education</h3>
+      </div>
+    )
+  }
+}
+
+class Experience extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Experience</h3>
+      </div>
+    )
+  }
 }
 
 export default Form;
