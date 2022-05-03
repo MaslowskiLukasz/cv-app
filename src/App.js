f8663d7f5ffabf8867e1620730be1df63247f153
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-05-03 13:08:57 +0200

    Add empty Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>CV-app</h1>
+        <Form />
       </div>
     );
   }
